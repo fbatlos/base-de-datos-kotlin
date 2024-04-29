193fe6057d8c31c0e3fb63d0bad525f5d5b4135a
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-04-29 17:09:25 +0200

    Connecxion base de datos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,23 +9,40 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.sql.DriverManager
+import java.sql.SQLException
 
 @Composable
 @Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
+fun App(text:String) {
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
+
             Text(text)
-        }
+
     }
 }
 
 fun main() = application {
+    var url = "jdbc:mysql://localhost:3306/studentdb"
+    val usuario = "studentuser"
+    val password = "password"
+
+    var texto = ""
+
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver")
+        val conexion = DriverManager.getConnection(url,usuario,password)
+
+        texto = "conexion exitosa"
+        conexion.close()
+    }catch (e:SQLException){
+        texto = "ERROR coxion = ${e.message}"
+    } catch (e:ClassNotFoundException ){
+        texto = "ERROR sql = ${e.message}"
+    }
+
     Window(onCloseRequest = ::exitApplication) {
-        App()
+        App(texto)
     }
 }
